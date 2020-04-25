86da68398d8d80d2b451f036041f5e63160f72f2
alex <camward@yandex.ru>
2020-04-25 22:12:35 +0400

    получить список всех книг

--- src/main/java/com/example/demo/controllers/BookController.java
+++ src/main/java/com/example/demo/controllers/BookController.java
@@ -2,7 +2,10 @@ package com.example.demo.controllers;
 
 import com.example.demo.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookController {
@@ -10,4 +13,8 @@ public class BookController {
     @Autowired
     BookRepository bookRepository;
 
+    @GetMapping("/books")
+    public List getAllBooks() {
+        return bookRepository.findAll();
+    }
 }
