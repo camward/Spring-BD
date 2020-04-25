386ab0f26d91a925ac03ee0a4a5457087b904983
alex <camward@yandex.ru>
2020-04-25 22:16:18 +0400

    создать запись книги

--- src/main/java/com/example/demo/controllers/BookController.java
+++ src/main/java/com/example/demo/controllers/BookController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Book;
 import com.example.demo.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -16,5 +20,10 @@ public class BookController {
     @GetMapping("/books")
     public List getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    @PostMapping("/books")
+    public Book createBook(@Valid @RequestBody Book book) {
+        return (Book) bookRepository.save(book);
     }
 }
