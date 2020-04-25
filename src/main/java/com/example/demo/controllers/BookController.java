773c9dd60491dcf33d58570d79357507e61f335d
alex <camward@yandex.ru>
2020-04-25 22:20:50 +0400

    получить запись книги по id

--- src/main/java/com/example/demo/controllers/BookController.java
+++ src/main/java/com/example/demo/controllers/BookController.java
@@ -3,13 +3,11 @@ package com.example.demo.controllers;
 import com.example.demo.models.Book;
 import com.example.demo.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -25,5 +23,10 @@ public class BookController {
     @PostMapping("/books")
     public Book createBook(@Valid @RequestBody Book book) {
         return (Book) bookRepository.save(book);
+    }
+
+    @GetMapping("/books/{id}")
+    public Optional getBookById(@PathVariable(value = "id") Long bookId) {
+        return bookRepository.findById(bookId);
     }
 }
